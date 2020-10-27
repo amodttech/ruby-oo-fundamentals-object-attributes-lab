287792b9ed99c1f45e5ddfba1dbf4779a506e948
Amodttech <aaron@amodttech.com>
2020-10-26 21:49:04 -0400

    took long enough

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def name
+        @name
+    end
+
+    def name=(alt_name)
+        @name = alt_name
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(alt_breed)
+        @breed = alt_breed
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+    def name
+        @name
+    end
+
+    def name=(alt_name)
+        @name = alt_name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(alt_job)
+        @job = alt_job
+    end
+    
+end
